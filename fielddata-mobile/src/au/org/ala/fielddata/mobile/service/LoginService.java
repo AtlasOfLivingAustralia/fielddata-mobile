16a1534304042378c6a8dad6f647e00ae33870f2
chrisala <chris.godwin.ala@gmail.com>
2012-08-21 07:16:28 +0000

    Changed settings to access the koala count portal on UAT.
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@80 6a6df900-09af-505f-f2bd-838eb8a94856

--- fielddata-mobile/src/au/org/ala/fielddata/mobile/service/LoginService.java
+++ fielddata-mobile/src/au/org/ala/fielddata/mobile/service/LoginService.java
@@ -33,9 +33,13 @@ public class LoginService extends WebServiceClient {
 	
 	public LoginResponse login() {
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-		params.set("portalName", "Condamine NRM");
+//		params.set("portalName", "Condamine NRM");
+//		params.set("username", "admin");
+//		params.set("password", "cambia401FD");
+//		
+		params.set("portalName", "Koala Count");
 		params.set("username", "admin");
-		params.set("password", "cambia401FD");
+		params.set("password", "password");
 		
 		
 		String url = serverUrl + loginUrl;

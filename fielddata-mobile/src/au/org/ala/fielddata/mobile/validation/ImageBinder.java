855e96b1ac3c2249cb3cb51f11ef90f2f3077a93
chrisala <chris.godwin.ala@gmail.com>
2012-08-24 02:22:52 +0000

    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@101 6a6df900-09af-505f-f2bd-838eb8a94856

--- fielddata-mobile/src/au/org/ala/fielddata/mobile/validation/ImageBinder.java
+++ fielddata-mobile/src/au/org/ala/fielddata/mobile/validation/ImageBinder.java
@@ -21,99 +21,115 @@ public class ImageBinder implements Binder {
 	private Attribute attribute;
 	private String thumbUri;
 	private boolean expectingResult;
-	
-	public ImageBinder(CollectSurveyData ctx, Attribute attribute, View imageView) {
+
+	public ImageBinder(CollectSurveyData ctx, Attribute attribute,
+			View imageView) {
 		this.ctx = ctx;
 		this.attribute = attribute;
 		expectingResult = false;
 		ctx.addImageListener(this);
-		thumb = (ImageView)imageView.findViewById(R.id.photoThumbnail);
+		thumb = (ImageView) imageView.findViewById(R.id.photoThumbnail);
 		SurveyViewModel model = ctx.getViewModel();
 		thumbUri = model.getRecord().getValue(attribute);
-		
+
 		updateThumbnail();
-		
+
 		addEventHandlers(imageView);
 	}
-	
+
 	private void addEventHandlers(View view) {
-		ImageButton takePhoto = (ImageButton)view.findViewById(R.id.takePhoto);
-		
+		ImageButton takePhoto = (ImageButton) view.findViewById(R.id.takePhoto);
+
 		takePhoto.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				Uri fileUri = StorageManager.getOutputMediaFileUri(StorageManager.MEDIA_TYPE_IMAGE);
+				Uri fileUri = StorageManager
+						.getOutputMediaFileUri(StorageManager.MEDIA_TYPE_IMAGE);
 				intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-				// Unfortunately, this URI isn't being returned in the result 
+				// Unfortunately, this URI isn't being returned in the result
 				// as expected so we have to eagerly bind it to our Record.
 				thumbUri = fileUri.toString();
 				bind();
 				expectingResult = true;
-				ctx.startActivityForResult(intent, CollectSurveyData.TAKE_PHOTO_REQUEST );
+				ctx.startActivityForResult(intent,
+						CollectSurveyData.TAKE_PHOTO_REQUEST);
 			}
 		});
-		
-		ImageButton selectFromGallery = (ImageButton)view.findViewById(R.id.selectFromGallery);
+
+		ImageButton selectFromGallery = (ImageButton) view
+				.findViewById(R.id.selectFromGallery);
 		selectFromGallery.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
 				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 				intent.setType("image/*");
 				expectingResult = true;
-				ctx.startActivityForResult(Intent.createChooser(intent, "Select Photo"), CollectSurveyData.SELECT_FROM_GALLERY_REQUEST);
-				
+				ctx.startActivityForResult(
+						Intent.createChooser(intent, "Select Photo"),
+						CollectSurveyData.SELECT_FROM_GALLERY_REQUEST);
+
 			}
 		});
-		
+
 		thumb.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
 				if (thumbUri == null || "".equals(thumbUri)) {
 					return;
 				}
-				Intent intent = new Intent(Intent.ACTION_VIEW);
-				intent.setData(Uri.parse(thumbUri));
-				ctx.startActivity(intent);
+				Uri imageUri = Uri.parse(thumbUri);
+				// Can't view files using the ACTION_VIEW.  Need to update
+				// the Photo taking to allow the media manager to scan it.
+				if ("content".equals(imageUri.getScheme())) {
+
+					Intent intent = new Intent(Intent.ACTION_VIEW, imageUri);
+
+					ctx.startActivity(intent);
+				}
 			}
 		});
-		
+
 	}
-	
+
 	private void updateThumbnail() {
 		if (thumbUri != null && "".equals(thumbUri) == false) {
-			
+
 			Uri uri = Uri.parse(thumbUri);
 			try {
+
+				// TODO fixme. Working with the full size bitmap in real time on the
+				// UI thread is bad....
 				
-				
-				Bitmap image = MediaStore.Images.Media.getBitmap(ctx.getContentResolver(), uri);
-				Bitmap imageThumb = Bitmap.createScaledBitmap(image, 100, 100, false);
-				//Bitmap image = BitmapFactory.decodeFile(uri.getEncodedPath());
+				Bitmap image = MediaStore.Images.Media.getBitmap(
+						ctx.getContentResolver(), uri);
+				Bitmap imageThumb = Bitmap.createScaledBitmap(image, 100, 100,
+						false);
+				// Bitmap image =
+				// BitmapFactory.decodeFile(uri.getEncodedPath());
 				thumb.setImageBitmap(imageThumb);
-			}
-			catch (Exception e) {
+			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
 		}
 	}
-	
+
 	public void onImageSelected(Uri imageUri) {
 		if (!expectingResult) {
 			return;
 		}
-		
+
 		// For some reason, the camera application passes back a null intent
 		// on my Galaxy Nexus, so we have to rely on the URI we created before
 		// starting the Camera activity.
 		if (imageUri != null) {
 			thumbUri = imageUri.toString();
 		}
-		
+
 		updateThumbnail();
 		expectingResult = false;
 	}
-	
+
 	public void bind() {
 		ctx.getViewModel().getRecord().setValue(attribute, thumbUri);
 	}

29359accdbaa1af1b7c35255ba9b068b1e9ee1b7
chrisala <chris.godwin.ala@gmail.com>
2012-08-29 03:48:11 +0000

    Put the login into a background thread.
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@117 6a6df900-09af-505f-f2bd-838eb8a94856

--- fielddata-mobile/src/au/org/ala/fielddata/mobile/LoginActivity.java
+++ fielddata-mobile/src/au/org/ala/fielddata/mobile/LoginActivity.java
@@ -2,11 +2,10 @@ package au.org.ala.fielddata.mobile;
 
 import java.util.List;
 
-import com.actionbarsherlock.app.SherlockActivity;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,51 +15,68 @@ import au.org.ala.fielddata.mobile.dao.GenericDAO;
 import au.org.ala.fielddata.mobile.model.Survey;
 import au.org.ala.fielddata.mobile.pref.Preferences;
 import au.org.ala.fielddata.mobile.service.FieldDataService;
-import au.org.ala.fielddata.mobile.service.LoginResponse;
 import au.org.ala.fielddata.mobile.service.LoginService;
+
+import com.actionbarsherlock.app.SherlockActivity;
 
 public class LoginActivity extends SherlockActivity implements OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        
-        Button button = (Button) findViewById(R.id.loginBtn);
-		button.setOnClickListener(this);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-    
+		Button button = (Button) findViewById(R.id.loginBtn);
+		button.setOnClickListener(this);
+	}
+
 	public void onClick(View v) {
 		if (v.getId() == R.id.loginBtn) {
-			EditText username = (EditText)findViewById(R.id.username);
-			EditText password = (EditText)findViewById(R.id.userPassword);
-			String portalName = new Preferences(this).getFieldDataPortalName();
-			
-			LoginService loginService = new LoginService(LoginActivity.this);
-			
-			try {
-				loginService.login(username.getText().toString(), password.getText().toString(), portalName);
-			
-				downloadSurveys();
-				// return to the main activity
-				finish();
-				
-			} catch(Exception e) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				builder.setTitle(R.string.login_failed);
-				builder.setMessage(R.string.unknown_user);
-				builder.setNegativeButton(R.string.close, new Dialog.OnClickListener() {
+			final EditText username = (EditText) findViewById(R.id.username);
+			final EditText password = (EditText) findViewById(R.id.userPassword);
+			final String portalName = new Preferences(this).getFieldDataPortalName();
 
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.cancel();
+			new AsyncTask<Void, Void, Void>() {
+				private Exception e;
+
+				public Void doInBackground(Void... args) {
+					LoginService loginService = new LoginService(LoginActivity.this);
+
+					try {
+						loginService.login(username.getText().toString(), password.getText()
+								.toString(), portalName);
+
+						downloadSurveys();
+						// return to the main activity
+						finish();
+
+					} catch (Exception e) {
+						this.e = e;
 					}
-				});
-				builder.show();
-			}
+					return null;
+				}
+
+				protected void onPostExecute(Void result) {
+					if (e != null) {
+
+						AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+						builder.setTitle(R.string.login_failed);
+						builder.setMessage(R.string.unknown_user);
+						builder.setNegativeButton(R.string.close, new Dialog.OnClickListener() {
+
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.cancel();
+							}
+						});
+						builder.show();
+					}
+				}
+
+			}.execute();
 		}
 	}
-	
+
 	private void downloadSurveys() {
 
 		FieldDataService service = new FieldDataService(LoginActivity.this);
